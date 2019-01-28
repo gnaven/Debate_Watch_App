8752dc8085d244de8aff24998d881d584297fbde
Gazi Mahir Ahmed Naven <naven.gman@gmail.com>
2019-01-27 20:51:56 -0500

    Changed UI and gets data in a txt file

--- Watch_App2/app/src/main/java/com/example/android/watch_app2/MainActivity.java
+++ Watch_App2/app/src/main/java/com/example/android/watch_app2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.android.watch_app2;
 
+import android.Manifest;
 import android.content.Context;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -22,8 +24,11 @@ import org.json.JSONObject;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.concurrent.TimeUnit;
 
 //TODO: Make a new class for actions using the buttons and call the class onCreate
@@ -36,6 +41,7 @@ public class MainActivity extends WearableActivity implements OnClickListener,Se
     public Button mStartButton;
     public Button mStopButton;
     public Button mResetButton;
+    public Button mSaveButton;
 
     public Button mPrevious;
 
@@ -45,7 +51,7 @@ public class MainActivity extends WearableActivity implements OnClickListener,Se
     final int MSG_RESET_TIMER = 3;
 
     Stopwatch timer = new Stopwatch();
-    final int REFRESH_RATE = 100;
+    final int REFRESH_RATE = 1000;
     public static float heartrate = 0.0f;
     // initializing accelerometer variables
     public static float acc_x;
@@ -62,6 +68,7 @@ public class MainActivity extends WearableActivity implements OnClickListener,Se
     private Sensor mAccelerometerSensor;
     private Sensor mGyroSensor;
     private Sensor mHeartRateSensor;
+    boolean sensorHrateRegistered;
 
     JSONObject data;
     JSONArray roundArr;
@@ -72,12 +79,12 @@ public class MainActivity extends WearableActivity implements OnClickListener,Se
         setContentView(R.layout.activity_main);
 
         // makes a new data file for storing data from the watch
-        data = new JSONObject();
-        try {
-            data.put("watch id", 101);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+//        data = new JSONObject();
+//        try {
+//            data.put("watch id", 101);
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
         roundArr = new JSONArray();
 
 
@@ -88,6 +95,8 @@ public class MainActivity extends WearableActivity implements OnClickListener,Se
         mStartButton = (Button)findViewById(R.id.start_button);
         mStopButton = (Button)findViewById(R.id.stop_button);
         mResetButton = (Button)findViewById(R.id.reset_button);
+        mSaveButton = (Button) findViewById(R.id.save_button);
+        mSaveButton.setVisibility(View.INVISIBLE);
 
         mStartButton.setOnClickListener(this);
         mStopButton.setOnClickListener(this);
@@ -99,11 +108,15 @@ public class MainActivity extends WearableActivity implements OnClickListener,Se
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mAccelerometerSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        //mGyroSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-        //mHeartRateSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
+        mGyroSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        mHeartRateSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
 
-        boolean sensorRegistered = mSensorManager.registerListener(this, mAccelerometerSensor, SensorManager.SENSOR_DELAY_FASTEST);
+        sensorHrateRegistered = mSensorManager.registerListener(this, mHeartRateSensor, SensorManager.SENSOR_DELAY_FASTEST);
 
+        //boolean sensorRegistered = mSensorManager.registerListener(this, mAccelerometerSensor, SensorManager.SENSOR_DELAY_FASTEST);
+
+        //makes a text file for storing data
+        newFile();
     }
 
 //    public void onClickStartButton(View v){
@@ -131,6 +144,9 @@ public class MainActivity extends WearableActivity implements OnClickListener,Se
 
         String nextString = "Next Clicked";
         Toast.makeText(this, nextString, Toast.LENGTH_LONG).show();
+        roundNum = mRoundTextView.getText().toString();
+        mHandler.sendEmptyMessage(MSG_RESET_TIMER);
+        newFile();
 
     }
 
@@ -153,6 +169,10 @@ public class MainActivity extends WearableActivity implements OnClickListener,Se
 
         String prevString = "Previous Clicked";
         Toast.makeText(this, prevString, Toast.LENGTH_LONG).show();
+
+        roundNum = mRoundTextView.getText().toString();
+        newFile();
+        mHandler.sendEmptyMessage(MSG_RESET_TIMER);
 
     }
     /*
@@ -196,10 +216,15 @@ public class MainActivity extends WearableActivity implements OnClickListener,Se
                     //mTimerText.setText(timeCurrent);
                     mTimerText.setText(""+timeCurrent);
                     try {
-                        sensorArr = sensorDataInput(sensorArr);
-                    } catch (JSONException e) {
+                        sensorDataInput(timeCurrent);
+                    } catch (IOException e) {
                         e.printStackTrace();
                     }
+//                    try {
+//                        sensorArr = sensorDataInput(sensorArr);
+//                    } catch (JSONException e) {
+//                        e.printStackTrace();
+//                    }
                     mHandler.sendEmptyMessageDelayed(MSG_UPDATE_TIMER,REFRESH_RATE); //text view is updated every second,
                     break;                                  //though the timer is still running
                 case MSG_STOP_TIMER:
@@ -211,7 +236,7 @@ public class MainActivity extends WearableActivity implements OnClickListener,Se
                                     TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS
                                             .toMinutes(timer.getElapsedTime()))
                     );
-                    mTimerText.setText(""+heartrate);
+                    mTimerText.setText(""+timeStop);
                     break;
 
                 default:
@@ -221,73 +246,160 @@ public class MainActivity extends WearableActivity implements OnClickListener,Se
     };
     JSONArray sensorArr;
     JSONObject roundN;
+    private FileWriter writer;
+    String roundNum = "1";
+    FileOutputStream outputStream;
     @Override
+
     public void onClick(View v) {
         /*
         Manages Buttons being clicked. Disables Stop when Start clicked and vice versa
-         */
+        */
+
         if(mStartButton == v) {
+            newFile();
             mStartButton.setEnabled(false);
+            mStartButton.setVisibility(View.INVISIBLE);
             mStopButton.setEnabled(true);
+            mStopButton.setVisibility(View.VISIBLE);
+            mSaveButton.setEnabled(false);
+            String StartString = "Start Clicked";
+            Toast.makeText(this, StartString, Toast.LENGTH_LONG).show();
+            //roundNum = mRoundTextView.getText().toString();
+            //File file = new File(getFilesDir(), "watch_data.txt");
+//            try {
+//                outputStream = openFileOutput("watch_data.txt", Context.MODE_PRIVATE);
+//            } catch (FileNotFoundException e) {
+//                e.printStackTrace();
+//            }
+
+//            try {
+//                writer = new FileWriter("watch_data.txt",true);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+
             mHandler.sendEmptyMessage(MSG_START_TIMER);
 
             // making a new Round Object
-            String roundNum = mRoundTextView.getText().toString();
-            roundN = new JSONObject();
-            try {
-                roundN.put("round num", roundNum);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            sensorArr = new JSONArray();
 
+//            roundN = new JSONObject();
+//            try {
+//                roundN.put("round num", roundNum);
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//            sensorArr = new JSONArray();
             startMeasure();
+
         }else
-        if(mStopButton == v){
+        if(mStopButton == v) {
             mStopButton.setEnabled(false);
+            mStopButton.setVisibility(View.INVISIBLE);
             mStartButton.setEnabled(true);
+            mStartButton.setVisibility(View.VISIBLE);
+            //mSaveButton.setEnabled(true);
             mHandler.sendEmptyMessage(MSG_STOP_TIMER);
-            try {
-                roundN.put("sensor data", sensorArr);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-            roundArr.put(roundN);
 
             try {
-                data.put("round num", roundArr);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            try {
-                fileClose();
+                outputStream.close();
+                System.out.println("file saved");
+                String saveString = "Stopped and Saved";
+                Toast.makeText(this, saveString, Toast.LENGTH_LONG).show();
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
+//            if(writer != null) {
+//                try {
+//                    writer.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//            try {
+//                outputStream.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+            //System.out.println(getFilesDir());
+//            try {
+//                roundN.put("sensor data", sensorArr);
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//
+//            roundArr.put(roundN);
+//
+//            try {
+//                data.put("round num", roundArr);
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//            try {
+//                fileClose();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
             stopMeasure();
+        }else if (mSaveButton ==v){
+            Button svButton = (Button)findViewById(R.id.save_button);
+            svButton.setEnabled(false);
+            try {
+                outputStream.close();
+                System.out.println("file saved");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }else if (mResetButton == v){
             mStartButton.setEnabled(true);
             mStopButton.setEnabled(true);
+
+            String resetString = "Reset & Data Deleted";
+            Toast.makeText(this, resetString, Toast.LENGTH_LONG).show();
+
+            String filename = "watch_data_round_"+roundNum+".txt";
+            File file = new File(getFilesDir()+"/"+filename);
+            if (file.exists()) {
+                file.delete();
+            }
+
             mHandler.sendEmptyMessage(MSG_RESET_TIMER);
         }
     }
 
+    public void newFile() {
+        String filename = "watch_data_round_"+roundNum+".txt";
+        if (new File(getFilesDir()+"/"+filename).exists()){
+            System.out.println("file already exists");
+            try {
+                outputStream = openFileOutput(filename, Context.MODE_APPEND);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }else {
+            try {
+                outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            System.out.println("new file made");
+        }
+    }
     private void startMeasure() {
         boolean sensorAccRegistered = mSensorManager.registerListener(this, mAccelerometerSensor, SensorManager.SENSOR_DELAY_FASTEST);
-        //boolean sensorGyroRegistered = mSensorManager.registerListener(this, mGyroSensor, SensorManager.SENSOR_DELAY_FASTEST);
-       // boolean sensorHrateRegistered = mSensorManager.registerListener(this, mHeartRateSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        boolean sensorGyroRegistered = mSensorManager.registerListener(this, mGyroSensor, SensorManager.SENSOR_DELAY_FASTEST);
+        //boolean sensorHrateRegistered = mSensorManager.registerListener(this, mHeartRateSensor, SensorManager.SENSOR_DELAY_FASTEST);
 
         Log.d("Sensor Status:", " Acc Sensor registered: " + (sensorAccRegistered ? "yes" : "no"));
-        //Log.d("Sensor Status:", " Gyro Sensor registered: " + (sensorGyroRegistered ? "yes" : "no"));
-        //Log.d("Sensor Status:", " Heart Rate Sensor registered: " + (sensorHrateRegistered ? "yes" : "no"));
+        Log.d("Sensor Status:", " Gyro Sensor registered: " + (sensorGyroRegistered ? "yes" : "no"));
+        Log.d("Sensor Status:", " Heart Rate Sensor registered: " + (sensorHrateRegistered ? "yes" : "no"));
 
     }
 
     private void stopMeasure() {
         mSensorManager.unregisterListener(this);
     }
-
 
     @Override
     public void onSensorChanged(SensorEvent event) {
@@ -305,7 +417,6 @@ public class MainActivity extends WearableActivity implements OnClickListener,Se
             heartrate = event.values[0];
         }
 
-
     }
 
     @Override
@@ -313,30 +424,35 @@ public class MainActivity extends WearableActivity implements OnClickListener,Se
 
     }
 
-    public JSONArray sensorDataInput (JSONArray sensorArray) throws JSONException {
-    // inputs all the fields as a JSON object
+//    public JSONArray sensorDataInput (JSONArray sensorArray) throws JSONException {
+//    // inputs all the fields as a JSON object
+//
+//        JSONObject sensorData = new JSONObject();
+//        sensorData.put("acc_x", acc_x);
+//        sensorData.put("acc_x", acc_y);
+//        sensorData.put("acc_x", acc_z);
+//        sensorData.put("gyro_x", gyro_x);
+//        sensorData.put("gyro_y", gyro_y);
+//        sensorData.put("gyro_z", gyro_z);
+//
+//        sensorArray.put(sensorData);
+//        return sensorArray;
+//    }
 
-        JSONObject sensorData = new JSONObject();
-        sensorData.put("acc_x", acc_x);
-        sensorData.put("acc_x", acc_y);
-        sensorData.put("acc_x", acc_z);
-        sensorData.put("gyro_x", gyro_x);
-        sensorData.put("gyro_y", gyro_y);
-        sensorData.put("gyro_z", gyro_z);
-
-        sensorArray.put(sensorData);
-
-        return sensorArray;
-
+    public void sensorDataInput(String time) throws IOException {
+        System.out.println("inputting data");
+        String fileContents = roundNum+","+time +","+acc_x+","+acc_y+","+acc_z+","+gyro_x+","+gyro_y+","+gyro_z+","+heartrate+"\n";
+        outputStream.write(fileContents.getBytes());
+        //writer.write( acc_x+","+acc_y+","+acc_z+"\n");
     }
 
-    public void fileClose () throws IOException {
-        String File_Name = "watch_data.JSON";
-        File datafile = new File (this.getFilesDir(), File_Name);
-        FileWriter fileWriter = new FileWriter(datafile.getAbsoluteFile());
-        BufferedWriter bw = new BufferedWriter(fileWriter);
-        bw.write(data.toString());
-        bw.close();
-
-    }
+//    public void fileClose () throws IOException {
+//        String File_Name = "watch_data.JSON";
+//        File datafile = new File (this.getFilesDir(), File_Name);
+//        FileWriter fileWriter = new FileWriter(datafile.getAbsoluteFile());
+//        BufferedWriter bw = new BufferedWriter(fileWriter);
+//        bw.write(data.toString());
+//        bw.close();
+//
+//    }
 }
